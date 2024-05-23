15bf80d1d88b1dc71a63daf4251cea62e3c6f20e
skyjo3 <skyjo3@gmail.com>
2024-05-23 14:44:48 -0700

    created photoclock detail css and updated the primary color on customized cursor

--- src/pages/projects/PhotoClockDetail.js
+++ src/pages/projects/PhotoClockDetail.js
@@ -1,13 +1,14 @@
 // PhotoClockDetail.js
 import React from 'react';
+import './PhotoClockDetail.css';
 
 const PhotoClockDetail = () => {
   return (
-    <div>
-      <h1>PhotoClock Project Detail</h1>
-      <img src="path/to/photoclock-image.jpg" alt="PhotoClock" />
-      <p>Description of the PhotoClock project.</p>
-      {/* Add more content as needed */}
+    <div className="container">
+      <div className="project-container">
+        <div>PhotoClock</div>
+        <p>description</p>
+      </div>
     </div>
   );
 };
