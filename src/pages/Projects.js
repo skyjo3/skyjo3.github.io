5829ad9a72057238c129659d8c40c62197709d4b
skyjo3 <skyjo3@gmail.com>
2024-05-23 14:34:05 -0700

    added a tag mechnism in the project list

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,5 @@
 // src/pages/Projects.js
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Projects.css';
 
 import { useLocation } from 'react-router-dom';
@@ -8,12 +8,45 @@ import ScrollIndicator from '../components/ScrollIndicator';
 import backgroundImage from '../assets/img/project_background.png';
 
 import { Link } from 'react-router-dom';
-import projects from './projectData';
+import projectsData from './projectData';
 
 const Projects = () => {
   // Get the current location using react-router-dom's useLocation hook
   const location = useLocation();
-  const isProjectsPage = location.pathname === '/projects'; // Change '/projects' to your actual Projects page route
+  const isProjectsPage = location.pathname === '/projects'; 
+
+  // Project data and tag filtering
+  const [projects, setProjects] = useState(projectsData); 
+  const [selectedTags, setSelectedTags] = useState(['all']);
+
+  // This effect runs when selectedTags changes
+  useEffect(() => {
+    const filteredProjects = selectedTags.includes('all') || selectedTags.length === 0
+      ? projectsData
+      : projectsData.filter(project =>
+          selectedTags.some(tag => project.tags.includes(tag))
+        );
+    setProjects(filteredProjects);
+  }, [selectedTags]); 
+
+  const handleTagClick = (tag) => {
+    setSelectedTags(prev => {
+        // If "all" is clicked, reset to only include "all"
+        if (tag === 'all') {
+            return ['all'];
+        }
+
+        // Remove "all" from selected tags if any other tag is clicked
+        const filteredTags = prev.filter(t => t !== 'all');
+
+        // Toggle the clicked tag
+        if (filteredTags.includes(tag)) {
+            return filteredTags.filter(t => t !== tag);
+        } else {
+            return [...filteredTags, tag];
+        }
+    });
+  };
 
   return (
     <div className="container" style={{ backgroundImage: `url(${backgroundImage})`, backgroundAttachment: 'fixed' }}>
@@ -23,28 +56,32 @@ const Projects = () => {
           <h5>↳  8 FEATURED PROJECTS</h5>
           <p>My works surrounds <b>human-photo interaction</b>, <b>UX design</b>, and <b>domestic artifacts</b>.</p>
           <p>Recently, I have started to incorporate <b>generative AI</b> to foster more <b>enriched and meaningful interactions</b> with personal data.</p>
-        </div>
-        {/* Display the first project separately */}
-        <div className="project project-1">
-          <Link to={projects[0].linkPath} className="project-link">
-            <div className="project-card">
-              <img src={projects[0].imageUrl} alt={projects[0].title} className="project-image" />
-              <div className="project-content">
-                <h2 className="project-title">{projects[0].title}</h2>
-                <div className="project-description">{projects[0].description}</div>
+          {/* Tags section */}
+          <div className="tags-container">
+            {['all', 'mobile', 'tangible', 'website', 'photos', 'audio', 'reading'].map(tag => (
+              <div key={tag}
+                   className={`tag ${selectedTags.includes(tag) ? 'selected' : ''}`}
+                   onClick={() => handleTagClick(tag)}>
+                {tag.charAt(0).toUpperCase() + tag.slice(1)}
               </div>
-            </div>
-          </Link>
+            ))}
+          </div>
         </div>
-        {/* Map through the rest of the projects */}
-        {projects.slice(1).map((project, index) => (
-          <div key={index} className={`project ${index % 2 === 0 ? 'projectL' : 'projectR'}`}>
+
+        {/* Filtered projects */}
+        {projects.map((project, index) => (
+          <div key={index} className={`project ${index % 2 === 0 ? 'projectR' : 'projectL'}`}>
             <Link to={project.linkPath} className="project-link">
               <div key={index} className="project-card">
                 <img src={project.imageUrl} alt={project.title} className="project-image" />
                 <div className="project-content">
                   <h2 className="project-title">{project.title}</h2>
                   <div className="project-description">{project.description}</div>
+                  <ul>
+                    {project.tags.filter(tag => tag !== 'all').map((tag, index) => (
+                      <li key={index}>{tag}</li>
+                    ))}
+                  </ul>
                 </div>
               </div>
             </Link>
--- src/pages/projectData.js
+++ src/pages/projectData.js
@@ -9,61 +9,70 @@ import image7 from '../assets/img/project-list/7-olo.jpg';
 import image8 from '../assets/img/project-list/8_olly.jpeg';
 import image9 from '../assets/img/project-list/9-slow-game.jpg';
 
-const projects = [
+const projectsData = [
   {
     title: 'PhotoClock',
     description: 'Resurfacing photos taken at this time of day in your past to trigger curiosity, anticipation, and intimacy.',
     imageUrl: image1,
     linkPath: '/projects/photoclock',
+    tags: ['photos', 'mobile', 'field study', 'all'],
   },
   {
     title: 'Chronoscope',
     description: 'Exploring photos through time, using tangible interaction to support curious discovery and reflection on life history.',
     imageUrl: image2,
     linkPath: '/projects/chronoscope',
+    tags: ['photos', 'tangible', 'field study', 'all'],
   },
   {
     title: 'CrescendoMessage',
     description: 'Investigating new forms of photo sharing between loved ones with an aim to enrich intimate communication.',
     imageUrl: image3,
     linkPath: '/projects/crescendo',
+    tags: ['photos', 'mobile', 'field study', 'all'],
   },
   {
     title: 'Womany',
     description: 'Unlocking self-growth and creativity: the news app redesigned for readers interested in gender equality.',
     imageUrl: image4,
     linkPath: '/projects/womany',
+    tags: ['reading', 'mobile', 'analytics', 'all'],
   },
   {
     title: 'Quologue',
     description: 'Reimagining everyday reading experience: AI-infused insights inspiring the rediscovery of cherished books.',
     imageUrl: image5,
     linkPath: '/projects/quologue',
+    tags: ['reading', 'website', 'field study', 'all'],
   },
   {
     title: 'Mental Map',
     description: 'Supporting visually impaired people to build mental map of a new environment via mobile interaction.',
     imageUrl: image6,
     linkPath: '/projects/mental-map',
+    tags: ['accessibility', 'mobile', 'contextual inquiry', 'all'],
   },
   {
     title: 'Olo Radio',
     description: 'Reflecting on personal listening history through tangible interaction with a domestic music player.',
     imageUrl: image7,
     linkPath: '/projects/olo',
+    tags: ['audio', 'tangible', 'field study', 'all'],
   },
   {
     title: 'Olly',
     description: 'Embodying one\'s music listening history data to invite memories in different life stages.',
     imageUrl: image8,
     linkPath: '/projects/olly',
+    tags: ['audio', 'tangible', 'field study', 'all'],
   },
   {
     title: 'Slow Game',
     description: 'Conceptualizing time alternatively by playing the Snake game one step every 18 hours.',
     imageUrl: image9,
     linkPath: '/projects/slow-game',
+    tags: ['temporality', 'tangible', 'research through design', 'all'],
   }
 ];
 
-export default projects;
+export default projectsData;
