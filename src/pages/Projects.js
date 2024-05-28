b235c24f3007a31de5b1639ada6dcb9a8437dd29
skyjo3 <skyjo3@gmail.com>
2024-05-28 15:28:28 -0700

    changing the project.css to be project.module.css to make sure styles are local and does not affect other pages

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,6 @@
 // src/pages/Projects.js
 import React, { useState, useEffect, useCallback } from 'react';
-import './Projects.css';
+import styles from './Projects.module.css';
 
 import { useLocation } from 'react-router-dom';
 import ScrollIndicator from '../components/ScrollIndicator';
@@ -77,20 +77,20 @@ const Projects = () => {
   }, [handleMouseEnter, handleMouseLeave]);
 
   return (
-    <div className="container">
-      <div className="projects-background-container">
+    <div className={styles["container"]}>
+      <div className={styles["projects-background-container"]}>
         <ScrollIndicator />
-        <div className="projects-container">
-          <div className="project intro">
-            <h5>↳  8 FEATURED PROJECTS</h5>
+        <div className={styles["projects-container"]}>
+          <div className={`${styles.project} ${styles.intro}`}>
+            <h5>↳  9 FEATURED PROJECTS</h5>
             <p>My works surrounds <b>human-photo interaction</b>, <b>UX design</b>, and <b>domestic artifacts</b>.</p>
             <p>Recently, I have started to incorporate <b>generative AI</b> to foster more <b>enriched and meaningful interactions</b> with personal data.</p>
             {/* Tags section */}
-            <div className="tags-container">
+            <div className={styles["tags-container"]}>
               {['all', 'mobile', 'tangible', 'website', 'photos', 'audio', 'reading'].map(tag => (
                 <div
                   key={tag}
-                  className={`tag ${selectedTags.includes(tag) ? 'selected' : ''}`}
+                  className={`${styles.tag} ${selectedTags.includes(tag) ? styles.selected : ''}`}
                   onClick={() => handleTagClick(tag)}
                   onMouseEnter={() => handleMouseEnter(tag)}
                   onMouseLeave={() => handleMouseLeave(tag)}
@@ -103,13 +103,13 @@ const Projects = () => {
 
           {/* Filtered projects */}
           {projects.map((project, index) => (
-            <div key={index} className={`project ${index % 2 === 0 ? 'projectR' : 'projectL'}`}>
-              <Link to={project.linkPath} className="project-link">
-                <div key={index} className="project-card">
-                  <img src={project.imageUrl} alt={project.title} className="project-image" />
-                  <div className="project-content">
-                    <h2 className="project-title">{project.title}</h2>
-                    <div className="project-description">{project.description}</div>
+            <div key={index} className={styles[`project${index % 2 === 0 ? 'R' : 'L'}`]}>
+              <Link to={project.linkPath} className={styles["project-link"]}>
+                <div key={index} className={styles["project-card"]}>
+                  <img src={project.imageUrl} alt={project.title} className={styles["project-image"]} />
+                  <div className={styles["project-content"]}>
+                    <h2 className={styles["project-title"]}>{project.title}</h2>
+                    <div className={styles["project-description"]}>{project.description}</div>
                     <ul>
                       {project.tags.filter(tag => tag !== 'all').map((tag, index) => (
                         <li key={index}>{tag}</li>
