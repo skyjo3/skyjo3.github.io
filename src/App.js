2d4d280e6b85c4cd43d663369f11f57be498d841
skyjo3 <skyjo3@gmail.com>
2024-06-05 18:02:27 -0700

    does not use the custom cursor when it's in mobile environment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import AppRouter from './AppRouter';
 import CustomCursor from './components/CustomCursor';
@@ -8,10 +8,22 @@ import './styles.css';
 import './variables.css';
 
 function App() {
+
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
     <>
       <AppRouter />
-      <CustomCursor />
+      {isDesktop && <CustomCursor />}
     </>
   );
 }
