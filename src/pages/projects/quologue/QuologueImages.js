e6f5043fc879327e7b3944d038d5134a5647c9f0
skyjo3 <skyjo3@gmail.com>
2024-06-13 13:59:05 -0700

    downsize images for quologue

--- src/pages/projects/quologue/QuologueImages.js
+++ src/pages/projects/quologue/QuologueImages.js
@@ -1,4 +1,4 @@
-import imageProject from './img/project.png'
+import imageProject from './img/project.jpg'
 import imageContext from './img/kindle.jpg'
 import imagePlan from './img/plan.png'
 import imageInterview from './img/interview.jpg'
@@ -7,24 +7,11 @@ import imageSitemap from './img/QuologueSitemap-AmyC.png'
 import imageMiro from './img/figma.png'
 import imageUserFlow from './img/QuologueUserflow-AmyC.png'
 
-import imageDesign1 from './img/design/loading.png'
+import imageDesign1 from './img/design/loading.jpg'
 import imageDesign2 from './img/design/welcome.jpg'
 import imageDesign3 from './img/design/keywords.jpg'
 import imageDesign4 from './img/design/historyview.png'
-import imageDesign5 from './img/design/3devices-2-01.png'
-
-// import imageMore1 from './img/more/0_main.png'
-// import imageMore2 from './img/more/0_x.PNG'
-// import imageMore3 from './img/more/0_y.PNG'
-// import imageMore4 from './img/more/1.PNG'
-// import imageMore5 from './img/more/2.PNG'
-// import imageMore6 from './img/more/3.PNG'
-// import imageMore7 from './img/more/4.PNG'
-// import imageMore8 from './img/more/5.PNG'
-// import imageMore9 from './img/more/6.jpg'
-// import imageMore10 from './img/more/7.PNG'
-// import imageMore11 from './img/more/8.PNG'
-// import imageMore12 from './img/more/9.jpg'
+import imageDesign5 from './img/design/3devices-2-01.jpg'
 
 const images = {
 
@@ -46,21 +33,6 @@ const images = {
     { src: imageDesign3, alt: 'Quologue Design' },
     { src: imageDesign4, alt: 'Quologue Use Scenario' },
   ],
-
-  // more: [
-  //   { src: imageMore1, alt: 'Quologue Design' },
-  //   { src: imageMore2, alt: 'Quologue Design' },
-  //   { src: imageMore3, alt: 'Quologue Design' },
-  //   { src: imageMore4, alt: 'Quologue Use Scenario' },
-  //   { src: imageMore5, alt: 'Quologue Design' },
-  //   { src: imageMore6, alt: 'Quologue Design' },
-  //   { src: imageMore7, alt: 'Quologue Design' },
-  //   { src: imageMore8, alt: 'Quologue Use Scenario' },
-  //   { src: imageMore9, alt: 'Quologue Design' },
-  //   { src: imageMore10, alt: 'Quologue Use Scenario' },
-  //   { src: imageMore11, alt: 'Quologue Use Scenario' },
-  //   { src: imageMore12, alt: 'Quologue Use Scenario' },
-  // ],
 };
 
 export default images;
