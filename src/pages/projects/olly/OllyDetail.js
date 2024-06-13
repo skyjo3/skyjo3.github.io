b176d9b3a59a65a7880ab02ad3935336f03471e5
skyjo3 <skyjo3@gmail.com>
2024-06-13 12:42:30 -0700

    updated the olly page

--- src/pages/projects/olly/OllyDetail.js
+++ src/pages/projects/olly/OllyDetail.js
@@ -9,6 +9,8 @@ import CustomSwiper from '../../../components/CustomSwiper';
 import Gallery from '../../../components/Gallery';
 import LazyLoadImage from '../../../components/LazyLoadImage';
 
+import { CommonNinjaWidget } from 'commonninja-react';
+
 import ProjectBottomCTA from '../../../components/ProjectBottomCTA'
 
 import images from './OllyImages'
@@ -19,6 +21,7 @@ const OllyDetail = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [activeSection, setActiveSection] = useState('');
+  const [loadCommonNinjaWidget, setLoadCommonNinjaWidget] = useState(false);
 
   const sectionRefs = useRef({
     overview: useRef(null),
@@ -41,6 +44,9 @@ const OllyDetail = () => {
       entries.forEach(entry => {
         if (entry.isIntersecting) {
           setActiveSection(entry.target.id);
+          if (entry.target.id === 'test') {
+            setLoadCommonNinjaWidget(true);
+          }
         }
       });
     }, 300); // Debounce delay of 300 milliseconds
@@ -93,10 +99,8 @@ const OllyDetail = () => {
           <ul>
               <li onClick={() => handleNavClick('overview')} className={activeSection === 'overview' ? styles.active : ''}>0 - overview</li>
               <li onClick={() => handleNavClick('research')} className={activeSection === 'research' ? styles.active : ''}>1 - context</li>
-              <li onClick={() => handleNavClick('empathize')} className={activeSection === 'empathize' ? styles.active : ''}>2 - interview</li>
-              <li onClick={() => handleNavClick('design')} className={activeSection === 'design' ? styles.active : ''}>3 - design + dev</li>
-              <li onClick={() => handleNavClick('test')} className={activeSection === 'test' ? styles.active : ''}>4 - field study</li>
-              <li onClick={() => handleNavClick('takeaways')} className={activeSection === 'takeaways' ? styles.active : ''}>5 - reflection</li>
+              <li onClick={() => handleNavClick('design')} className={activeSection === 'design' ? styles.active : ''}>2 - design</li>
+              <li onClick={() => handleNavClick('test')} className={activeSection === 'test' ? styles.active : ''}>3 - documentation</li>
           </ul>
         </div>
         <div className={styles.content}>
@@ -106,140 +110,91 @@ const OllyDetail = () => {
               <div className={styles.overviewContainer}>
                 <div>
                   <h2>Overview</h2>
-                  <p>Olly is an Generative AI-empowered website that allows people to rediscover and interact with their e-book highlights. Offering thought-provoking prompts tailored to individual knowledge preferences, users reconnect with significant content, documenting reflections to advance their understanding.</p>
+                  <p>Olly is a domestic music player that replays songs people have listened to. It occasionally picks a song from the owner’s Last.FM listening history and spins in a speed based on how long ago the song was played. Once the user notices the spinning, they can choose to replay this song by rotating the circular disk. This project’s contribution is to explore how to create a slow expression that invites people to revisit their personal memories through an embodiment of temporal metadata.</p>
                 </div>
                 <div>
                   <h2>Team</h2>
-                  <h6>Lead Software Engineer & Design Research Mentor</h6>Amy Yo Sue Chen
-                  <h6>Project Lead, Ideation, Design, Research‍</h6>Sol Kang
-                  <h6>Design & Research Supervisor</h6>William Odom
-                  <h6>Software Engineering</h6>Vlad Vakulenko
+                  <h6>Project Lead</h6>William Odom, <br />Ron Wakkary
+                  <h6>Research Assistant on Paper + LaTex</h6>Amy Yo Sue Chen
+                  <h6>Design + Implementation</h6>Jeroen Hol, <br />Bram Naus, <br />Pepijn Verburg
+                  <h6>Illustration</h6>Tal Amram
                 </div>
               </div>
               <div>
                 <h2>Contribution / Impact</h2>
                 <ul>
-                  <li>This project leverages AI to elevate digital reading, transforming passive consumption into a dynamic, conversational experience that enhances understanding and expands perspectives.</li>
-                  {/*<li></li>*/}
+                  <li>One of its research paper has won the <b>Best Paper Award</b> in ACM CHI 2019, the top HCI conference.</li>
+                  <li>This work has been recognized and cited by <b>Spotify's research team</b> in their paper, <a href="http://doi.org/10.1145/3313831.3376493">"Giving Voice to Silent Data: Designing with Personal Music Listening History"</a> in ACM CHI 2020.</li>
+                  <li>Re-experiencing past music allows users to reflect on life stages, enhancing their appreciation of digital data for personal history and identity.</li>
+                  <li>Introducing 'slowness'—delays and reduced control—enriches interactions, suggesting technologies focus on long-term engagement over quick consumption.</li>
                 </ul>
               </div>
             </section>
             <section id="research" ref={sectionRefs.current.research}>
-                <LazyLoadImage src={images.singles.imageContext.src} alt={images.singles.imageContext.alt} className={styles.contextImage} onClick={() => openLightbox(images.singles.imageContext.src)} />
+                <CustomSwiper images={images.context} />
                 <h2>1 - Research Context</h2>
                 <div className={styles.researchContainer}>
                   <div>
                     <h3>Problem Statement</h3>
-                    <p>In today's digital landscape, <b>eBooks provide vast information but often lead to passive consumption</b>. There is a pressing need for a platform that not only facilitates access to digital content but also promotes active engagement and reflection, helping users challenge their beliefs and expand their perspectives through thoughtful dialogue.</p>
+                    <p>The pervasive use of digital technologies often prioritizes efficiency and rapid interaction, leading to shallow engagement with personal data. Also, existing designs generally overlook how technology can facilitate meaningful, long-term experiences with personal digital archives, particularly music history.</p>
                   </div>
                   <div>
                     <h3>Research Goal</h3>
-                    <p>The project seeks to foster an <b>open dialogue</b>, encouraging users to <b>revisit and challenge</b> their own perspectives. Through seamlessly integrating AI in the background, we aim to facilitate a sophisticated conversation between individuals and their insights, pushing the boundaries of <b>how AI can elegantly support and enhance these meaningful interactions</b>.</p>
+                    <p>This study aims to explore how "slowness" as a design strategy in technology can enhance deep, reflective interaction with personal digital music archives, fostering sustained user engagement and promoting richer, long-term experiences with technology in daily life.</p>
                   </div>
                 </div>
+                <h4>Research Questions</h4>
+                <ul>
+                <li>How can experiences of reminiscence and reflection on personal histories of digital music listening be supported through the design of a new kind of music player?</li>
+                <li>How might slowness, as a design strategy, enable emergent and ongoing experiences of music listening commensurate with a lifetime of one’s personal history of digital music?</li>
+                </ul>
                 <div className={styles.skillsContainer}>
                   <div>
                     <h4>research methods</h4>
                     <ul>
                       <li>Semi-structured Interviews</li>
                       <li>Field study</li>
-                      <li>Survey</li>
                       <li>Thematic analysis</li>
-                      <li>Google analytics</li>
                     </ul>
                   </div>
                   <div>
-                    <h4>website development</h4>
+                    <h4>digital fabrication</h4>
                     <ul>
-                      <li>HTML5, CSS3, JavaScript (React/Node.js)</li>
-                      <li>Amazon AWS S3, Vercel</li>
-                      <li>Google Authentication Login</li>
-                      <li>Github version control</li>
+                      <li>Last.FM data (from Spotify, Tidal, Youtube)</li>
+                      <li>wood</li>
+                      <li>aluminum</li>
                     </ul>
-                  </div>
-                  <div>
-                    <h4>design tools</h4>
-                    <ul>
-                      <li>Figma</li>
-                      <li>Miro</li>
-                      <li>Adobe Suite</li>
-                    </ul>
-                  </div>
-                </div>
-                <LazyLoadImage src={images.singles.imagePlan.src} alt={images.singles.imagePlan.alt} className={styles.planImage} onClick={() => openLightbox(images.singles.imagePlan.src)} />
-            </section>
-            <section id="empathize" ref={sectionRefs.current.empathize}>
-                <LazyLoadImage src={images.singles.interview.src} alt={images.singles.interview.alt} className={styles.interviewImage} onClick={() => openLightbox(images.singles.interview.src)} />
-                <h2>2 - Semi-Structured Interview</h2>
-                <p>To begin with, we conducted a semi-structured interview with <b>5 participants who are avid ebook readers</b>. We aim to understand their ebook reading practices, user experiences, and ideas on how ebook reading experiences could be enhanced.</p>
-                <p>Click the image below to see the interview protocol and question list in full screen:</p>
-                <LazyLoadImage src={images.singles.protocol.src} alt={images.singles.protocol.alt} className={styles.interviewImage} onClick={() => openLightbox(images.singles.protocol.src)} />
-                <h3>Findings + Corresponding Design Goals</h3>
-                <table border="1">
-                  <tr>
-                      <td><u>1. Annotations for Enriching Reading Experiences</u>:
-                      Participants' annotation habits primarily facilitate <b>fact-checking, memory aid, and emotional grounding</b>. Annotations serve as a versatile tool, enriching reading experiences and providing <b>personalized insights</b>.</td>
-                      <td><u>2. Reasons for Re-reading</u>:
-                      Participants re-read books for varied motivations, such as <b>emotional solace, professional clarity,</b> and preparation for discussions or movie adaptations. They <b>discover new details</b> and experience <b>evolving perceptions</b> over time.</td>
-                  </tr>
-                  <tr>
-                      <td><u>3. Sharing is Intimate</u>:
-                      While <b>sharing highlights</b> in a book club setting is beneficial for fostering <b>deeper connections and discussions</b>, some participants refrain from sharing intimate highlights as they are sometimes <b>very personal</b>.</td>
-                      <td><u>4. Stronger Attachment to Physical Books</u>:
-                      Participants expressed a <b>stronger personal attachment to physical books over ebooks</b>, citing the tactile and sensory experiences they provide.</td>
-                  </tr>
-                  <tr>
-                      <td colspan="2"><u>5. Potential Design Features</u>:
-                      One participant suggested <b>incorporating GPT-generated keywords</b> to highlight themes and character experiences, which could enhance discussions in book clubs and make social sharing more engaging.</td>
-                  </tr>
-                </table>
-                <p>After digesting these findings and prioritizing their importance, we have made 2 key decisions. First, in alignment with our research goals and respecting Finding #3's insight into the intimate nature of sharing, we have decided <i>not to pursue the social sharing feature</i>. Second, although Finding #4 presents an intriguing opportunity to recreate tangible and sensory interactions with digital highlights, we have deemed <i>other findings to be more feasible and significant for further development</i>.</p>
-                <p>Therefore, we have set our <b>design goals</b> to:</p>
-                <ul>
-                    <li>Utilize annotations as design resources to offer <b>variations of content</b> that users have already marked as important. This aims to entice curiosity and mimic evolving perceptions.</li>
-                    <li>Incorporate <b>GPT-generated keywords</b> to highlight themes and character experiences.</li>
-                    <li>Create in-depth <b>dialogical interactions</b> between the user and generative AI.</li>
-                </ul>
-                <div className={styles.funfactContainer}>
-                  <div></div>
-                  <div>
-                    <h5>Fun Fact</h5>
-                    <p>From this stage, we have also renamed our project to 'Olly' to better emphasize the integration of 'quotes' and 'dialogue'.</p>
                   </div>
                 </div>
             </section>
             <section id="design" ref={sectionRefs.current.design}>
-              <CustomSwiper images={images.design} />
-              <h2>3 - Proposed Design</h2>
-              <p>After the brainstorming session, we decided to have <b>5 key features</b> of Olly:</p>
-              <ol>
-                <li>a <b>website-based</b> platform that could take customized .csv files or Amazon Kindle's .txt files as input,</li>
-                <li>a responsive website that could make use of <b>ChatGPT's official APIs</b>,</li>
-                <li>a <b>conversation</b> that invites user to guess what quotes they have highlighed in the past,</li>
-                <li>a <b>reflective history page</b> that triggers user to extend their thinking about AI's remix, and </li>
-                <li>a <b>keyword setting</b> feature that enables user to prime the next interaction.</li>
-              </ol>
-              <p>To achieve these features, we diagrammed the Sitemap and User Flow. Click to see the details:</p>
-              <LazyLoadImage src={images.singles.sitemap.src} alt={images.singles.sitemap.alt} className={styles.sitemapImage} onClick={() => openLightbox(images.singles.sitemap.src)} />
-              <LazyLoadImage src={images.singles.userFlow.src} alt={images.singles.userFlow.alt} className={styles.userFlowImage} onClick={() => openLightbox(images.singles.userFlow.src)} />
-              <p>As the screenshot below shows, later we prototyped and designed our Olly website on Figma, and developed the website by React.js. We use Amazon AWS S3 to store the user data securely and host the website on Vercel.</p>
-              <LazyLoadImage src={images.singles.imageMiro.src} alt={images.singles.imageMiro.alt} className={styles.miroImage} onClick={() => openLightbox(images.singles.imageMiro.src)} />
-                
+              <h2>2 - Olly Design</h2>
+
+              {/*Video*/}
+              <div style={{ padding: "56.25% 0 0 0", position: "relative" }}>
+                <iframe 
+                  src="https://player.vimeo.com/video/130874655?h=f60e1a4d09&byline=0&portrait=0" 
+                  style={{ position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}
+                  frameBorder="0"
+                  allow="autoplay; fullscreen; picture-in-picture"
+                  allowFullScreen
+                ></iframe>
+              </div>
+              <script src="https://player.vimeo.com/api/player.js"></script>
+              {/*End of Video*/}
+              
+              <h3>More Images</h3>
+              <Gallery images={images.more} rows={4} columns={5} />
             </section>
             <section id="test" ref={sectionRefs.current.test}>
-                <h2>4 - Field Study</h2>
-                <p>Next, we decided to recruit <b>10 participants</b> in North America to join our <b>8-week</b> field deployment study. To ensure the robustness of Olly, our team members have used our own Kindle data to interact with the system before deployment.</p>
-                <p>The field study is currently running. It will end in mid June 2024, and we aim to have an one-hour long closing interview with them to learn their experience of using Olly. More details will be shared once we publish our results in a formal research paper.</p>
-            </section>
-            <section id="takeaways" ref={sectionRefs.current.takeaways}>
-                <h2>5 - Overall Reflection</h2>
-                <h3>How Research Changed the Design Direction</h3>
-                <p>This research has significantly influenced our design direction, informed by insights shared by participants. It has uncovered evidence supporting design guidelines aimed at addressing specific user needs and enhancing the reading experience by making it continuous, generative, inviting, and conducive to advancing knowledge acquisition to the next level.</p>
-                <p>To-Be Continued :)</p>
-                <div>
-                  {/*<h3>More Images</h3>*/}
-                  {/*<Gallery images={images.more} rows={2} columns={5} />*/}
-                </div>
+                <h2>3 - Documentation</h2>
+                <p>Next, we deployed Olly in <b>3 homes for 15 months </b> to explore how its slow pace might support experi- ences of reflection and reminiscence.</p>
+                <p>Findings and more details about participants' precious stories can be found in our paper:</p>
+                
+                {/* Conditionally load CommonNinjaWidget */}
+                {loadCommonNinjaWidget && (
+                  <CommonNinjaWidget widgetId="b3c8df6a-1324-45b3-8dda-dc2dd545fd65" />
+                )}
             </section>
 
             {/* Lightbox modal */}
--- src/pages/projects/olly/OllyImages.js
+++ src/pages/projects/olly/OllyImages.js
@@ -1,66 +1,36 @@
-import imageProject from './img/project.png'
-import imageContext from './img/kindle.jpg'
-import imagePlan from './img/plan.png'
-import imageInterview from './img/interview.jpg'
-import imageProtocol from './img/protocol.png'
-import imageSitemap from './img/QuologueSitemap-AmyC.png'
-import imageMiro from './img/figma.png'
-import imageUserFlow from './img/QuologueUserflow-AmyC.png'
+import imageProject from './img/3_olly.jpeg'
 
-import imageDesign1 from './img/design/loading.png'
-import imageDesign2 from './img/design/welcome.jpg'
-import imageDesign3 from './img/design/keywords.jpg'
-import imageDesign4 from './img/design/historyview.png'
-import imageDesign5 from './img/design/3devices-2-01.png'
+import imageContext1 from './img/context/context1.png'
+import imageContext2 from './img/context/context2.jpg'
+import imageContext3 from './img/context/context3.png'
+import imageContext4 from './img/context/context4.png'
 
-// import imageMore1 from './img/more/0_main.png'
-// import imageMore2 from './img/more/0_x.PNG'
-// import imageMore3 from './img/more/0_y.PNG'
-// import imageMore4 from './img/more/1.PNG'
-// import imageMore5 from './img/more/2.PNG'
-// import imageMore6 from './img/more/3.PNG'
-// import imageMore7 from './img/more/4.PNG'
-// import imageMore8 from './img/more/5.PNG'
-// import imageMore9 from './img/more/6.jpg'
-// import imageMore10 from './img/more/7.PNG'
-// import imageMore11 from './img/more/8.PNG'
-// import imageMore12 from './img/more/9.jpg'
+
+function importAll(r) {
+  let images = [];
+  r.keys().map((item, index) => { images.push({ src: r(item), alt: 'Olly Use Scenario' }); });
+  return images;
+}
+
+// Assuming your images are stored in './img/more' and have appropriate extensions.
+const imagesMore = importAll(require.context('./img/more', false, /\.(png|jpe?g|svg|JPG)$/));
+
+
 
 const images = {
 
   singles: {
     imageProject:    { src: imageProject, alt: 'Olly Design' },
-    imageContext:    { src: imageContext, alt: 'Olly Design' },
-    imagePlan:       { src: imagePlan, alt: 'Olly Design' },
-    interview:       { src: imageInterview, alt: 'Olly Use Scenario' },
-    protocol:        { src: imageProtocol, alt: 'Olly Use Scenario' },
-    sitemap:         { src: imageSitemap, alt: 'Olly Sitemap'},
-    imageMiro:       { src: imageMiro, alt: 'Olly Use Scenario' },
-    userFlow:        { src: imageUserFlow, alt: 'Olly Use Scenario' },
   },
 
-  design: [
-    { src: imageDesign5, alt: 'Olly Use Scenario' },
-    { src: imageDesign1, alt: 'Olly Design' },
-    { src: imageDesign2, alt: 'Olly Design' },
-    { src: imageDesign3, alt: 'Olly Design' },
-    { src: imageDesign4, alt: 'Olly Use Scenario' },
+  context: [
+    { src: imageContext1, alt: 'Olly Context' },
+    { src: imageContext2, alt: 'Olly Context' },
+    { src: imageContext3, alt: 'Olly Context' },
+    { src: imageContext4, alt: 'Olly Context' },
   ],
 
-  // more: [
-  //   { src: imageMore1, alt: 'Olly Design' },
-  //   { src: imageMore2, alt: 'Olly Design' },
-  //   { src: imageMore3, alt: 'Olly Design' },
-  //   { src: imageMore4, alt: 'Olly Use Scenario' },
-  //   { src: imageMore5, alt: 'Olly Design' },
-  //   { src: imageMore6, alt: 'Olly Design' },
-  //   { src: imageMore7, alt: 'Olly Design' },
-  //   { src: imageMore8, alt: 'Olly Use Scenario' },
-  //   { src: imageMore9, alt: 'Olly Design' },
-  //   { src: imageMore10, alt: 'Olly Use Scenario' },
-  //   { src: imageMore11, alt: 'Olly Use Scenario' },
-  //   { src: imageMore12, alt: 'Olly Use Scenario' },
-  // ],
+  more: imagesMore,
 };
 
 export default images;
