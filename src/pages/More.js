f8913895bad5e93307065eef35dcfb6f18100060
skyjo3 <skyjo3@gmail.com>
2024-06-24 17:48:10 -0700

    updated more page

--- src/pages/More.js
+++ src/pages/More.js
@@ -2,29 +2,45 @@
 import React from 'react';
 import styles from './More.module.css';
 
-import Lottie from 'react-lottie';
-import animationData from '../assets/lottie/Animation - 1717195759498.json';
+// import Lottie from 'react-lottie';
+// import animationData from '../assets/lottie/Animation - 1717195759498.json';
+
+import LazyLoadImage from '../components/LazyLoadImage';
+import images from './MoreImages'
 
 const More = () => {
 
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice' // Supports responsiveness
-    }
-  };
+  // const defaultOptions = {
+  //   loop: true,
+  //   autoplay: true,
+  //   animationData: animationData,
+  //   rendererSettings: {
+  //     preserveAspectRatio: 'xMidYMid slice' // Supports responsiveness
+  //   }
+  // };
 
   return (
     <div className={styles["container"]}>
       <div className={styles["more-background-container"]}>
         <div className={styles["more-container"]}>
-          <div className={styles["lottie"]}>
+          {/*<div className={styles["lottie"]}>
             <Lottie options={defaultOptions} />
+          </div>*/}
+        
+          <div className={styles["image-container"]}>
+            {/*<LazyLoadImage src={images.singles.img1.src} alt={images.singles.img1.alt} className={styles.moreImage} />*/}
+            <div className={styles["text-overlay"]}>
+              <h1>Let's collaborate on fun projects!</h1>
+              <p>Please contact me via this email: <br /><a href="mailto:amy.yosue.chen@gmail.com">amy.yosue.chen@gmail.com</a></p>
+            </div>
           </div>
-          <h1>contact</h1>
-          <h2><a href="mailto:amy.yosue.chen@gmail.com">amy.yosue.chen@gmail.com</a></h2>
+
+          <LazyLoadImage src={images.singles.img2.src} alt={images.singles.img2.alt} className={styles.moreImage} />
+          
+          <LazyLoadImage src={images.singles.img3.src} alt={images.singles.img3.alt} className={styles.moreImage} />
+          
+          <LazyLoadImage src={images.singles.img4.src} alt={images.singles.img4.alt} className={styles.moreImage} />
+          
         </div>
       </div>
     </div>
---
+++ src/pages/MoreImages.js
@@ -0,0 +1,16 @@
+import img1 from '../assets/img/more-list/more-01.png'
+import img2 from '../assets/img/more-list/more-02.png'
+import img3 from '../assets/img/more-list/more-03.png'
+import img4 from '../assets/img/more-list/more-04.png'
+
+const images = {
+
+  singles: {
+    img1:    { src: img1, alt: 'contact me' },
+    img2:    { src: img2, alt: 'I play softball' },
+    img3:    { src: img3, alt: 'I get things done' },
+    img4:    { src: img4, alt: 'I care about people' },
+  },
+};
+
+export default images;
