bba98ace60535fd8171c1958a95d087db518eef1
skyjo3 <skyjo3@gmail.com>
2024-09-30 14:42:46 -0700

    changed the text

--- src/pages/About.js
+++ src/pages/About.js
@@ -13,7 +13,7 @@ const About = () => (
       <div className="about-content">
         <h6>HELLO !</h6>
         <h1>My name is Amy.</h1>
-        <h2>I am a Software Engineer.</h2>
+        <h2>I am a Design Researcher.</h2>
         <p>I design, develop, and ship products with an aim to engage people with their own <b>vast and still growing personal data</b>, such as digital photos, music listening history, and ebook reading metadata.</p>
         <p>My work in Human-Computer Interaction (HCI) focuses on <b>interaction design</b>, <b>design prototyping</b>, and <b>data visualization</b>.</p>
         <Link to="/projects" className="custom-button">View My Projects</Link>
