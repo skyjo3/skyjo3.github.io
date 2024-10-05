1f68b8b2761877c0a3a420da4fdd873835480750
skyjo3 <skyjo3@gmail.com>
2024-10-05 11:47:49 -0700

    revised tags in project list

--- src/pages/About.js
+++ src/pages/About.js
@@ -14,7 +14,7 @@ const About = () => (
         <h6>HELLO !</h6>
         <h2>My name is Amy. I am a Design Researcher focused on Visual Media.</h2>
         <p>I design, develop, and ship products with an aim to engage people with their own <b>vast and still growing digital possessions</b>.</p>
-        <p>My work in Human-Computer Interaction (HCI) focuses on <b>interaction design</b>, <b>research product</b>, and <b>data wayfaring</b>.</p>
+        <p>My work in Human-Computer Interaction (HCI) focuses on <b>interaction design</b>, <b>research product</b>, and <b>data visualization</b>.</p>
         <Link to="/projects" className="custom-button">View My Projects</Link>
       </div>
     </div>
