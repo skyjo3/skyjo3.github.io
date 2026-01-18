2cb2d92413bdec6bada5d65d2d445c785a6f67ca
skyjo3 <skyjo3@gmail.com>
2026-01-17 16:03:18 -0800

    adjust greeting at home page

--- src/components/MorphingText.js
+++ src/components/MorphingText.js
@@ -1,10 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import './MorphingText.css';
 
+const getGreeting = () => {
+  const hour = new Date().getHours();
+  if (hour >= 5 && hour < 12) {
+    return 'Good morning! ☀️';
+  } else if (hour >= 12 && hour < 17) {
+    return 'Good afternoon! ☀️';
+  } else {
+    return 'Good evening! 🌙';
+  }
+};
+
 const MorphingText = () => {
-  const texts = ['visual media', 'photo & video']; // List of words
+  const texts = ['humans', 'aesthetics']; // List of words
   const [currentText, setCurrentText] = useState(texts[0]); // Set initial text
   const [index, setIndex] = useState(0); // Track current index
+  const [greeting, setGreeting] = useState(getGreeting());
 
   useEffect(() => {
     // Set interval to change the text every 2 seconds (2000ms)
@@ -21,9 +33,14 @@ const MorphingText = () => {
   }, [index, texts]);
 
   return (
-    <p className="landing-paragraph">
-      Amy Chen, a <b>Design Researcher</b> focused on <span className="morphing-text">{currentText}</span>.
-    </p>
+    <div>
+      <p className="landing-paragraph">
+      {greeting} 
+      </p>
+      <p className="landing-paragraph">
+        I am a <b>design engineer</b> who codes for <span className="morphing-text">{currentText}</span>.
+      </p>
+    </div>
   );
 };
 
